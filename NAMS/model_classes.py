23b3c31ae709186b87d0c826fe1b7a09add441bf
peturdanielthordarson <petur19@ru.is>
2019-12-02 15:22:54 +0000

    herna er destination klassin

--- NAMS/model_classes.py
+++ NAMS/model_classes.py
@@ -211,3 +211,46 @@ class Employee:
 			if ssn_str[:2] not in [ str(day) for day in range(1, 30)]:
 				pass
 
+class Crew:
+    def __init__(self):
+        self.__headpilot = ''
+        self.__copilot = ''
+        self.__headsteward = ''
+        self.__otherstewards = []
+
+    def set_headpilot(self, name_str):
+        if name_str.replace(' ', '').isalpha() and len(name_str) <= 60:
+            self.__headpilot = name_str
+            return True
+        else:
+            return False
+
+    def get_headpilot(self):
+        return self.__headpilot
+
+    def set_copilot(self, name_str):
+        if name_str.replace(' ', '').isalpha() and len(name_str) <= 60:
+            self.__copilot = name_str
+            return True
+        else:
+            return False
+
+    def get_copilot(self):
+        return self.__copilot
+
+    def set_headsteward(self, name_str):
+        if name_str.replace(' ', '').isalpha() and len(name_str) <= 60:
+            self.__headsteward = name_str
+            return True
+        else:
+            return False
+
+    def get_headsteward(self):
+        return self.__headsteward
+
+    def set_stewards(self, name_str):
+        if name_str.replace(' ', '').isalpha() and len(name_str) <= 60:
+            self.__otherstewards.append(name_str)
+            return True
+        else:
+            return False
