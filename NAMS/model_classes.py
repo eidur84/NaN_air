0c17678cf16b01423bae653064a81a925c1d7348
Pecky95 <danielpall3@gmail.com>
2019-12-02 15:24:36 +0000

    also airplane class

--- NAMS/model_classes.py
+++ NAMS/model_classes.py
@@ -1,4 +1,4 @@
-class Rtrip ():
+class Rtrip():
     def __init__(self):
         self.__valid_bool = True 
         self.__dest_str = ""
@@ -10,7 +10,7 @@ class Rtrip ():
         self.__return_date_str = ""
 
     def valid_bool(self, valid_bool):
-        
+        pass
 
     def set_dest_str(self, dest_str):
         if dest_str.replace(" ","").isalpha():
@@ -81,3 +81,38 @@ class Rtrip ():
         
     def get_return_date_str(self):
         return self.__return_date_str
+
+class Airplane():
+    self.__seat_count_int = ""
+    self.__type_str = ""
+    self.__manuf_str = ""
+
+    def set_seat_count_int(self, seat_count_int):
+        if seat_count_int.isdecimal:
+            self.__seat_count_int = seat_count_int:
+            return True
+        else:
+            return False
+
+    def get_seat_count_int(self):
+        return self.__seat_count_int
+
+    def set_type_str(self, type_str):
+        if type_str.replace(" ","").isalpha():
+            self.__type_str = type_str
+            return True
+        else:
+            return False
+    
+    def get_type_str(self):
+        return self._type_str
+
+    def set_manuf_str(self, manuf_str):
+        if manuf_str.replace(" ","").isalpha():
+            self.__manuf_str = manuf_str
+            return True
+        else:
+            return False
+
+    def get_manuf_str(self):
+        return self.__manuf_str
