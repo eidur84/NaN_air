1980e2671127adf9a9669601fcbaa6b9a9d189f2
Pecky95 <danielpall3@gmail.com>
2019-12-03 11:53:43 +0000

    dsf

---
+++ NAMS/DBLayer/invalidate class.py
@@ -0,0 +1,6 @@
+class Invalidate():
+    def __init__(self):
+        self.__invalidate_staff_str = ""
+        self.__invalidate_airplane_str = ""
+        self.__invalidate_dest_str = ""
+        self.__invalidate_rtrip_str = ""
