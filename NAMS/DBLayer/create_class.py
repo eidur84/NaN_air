0e695e14bf990d5cd83368761308f40973761f80
Pecky95 <danielpall3@gmail.com>
2019-12-03 11:58:34 +0000

    dsf

---
+++ NAMS/DBLayer/create_class.py
@@ -0,0 +1,7 @@
+class Create:
+    def __init__(self):
+        self.__new_data_str = ""
+        self.__create_staff_str = ""
+        self.__create_airplane_str = ""
+        self.__create_dest_str = ""
+        self.__create_rtrip_str = ""
