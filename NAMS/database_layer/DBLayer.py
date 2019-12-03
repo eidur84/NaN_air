c79752de4132df86fbf465b430f0196c2aa23f2e
Logi Eyjólfsson <logi19@ru.is>
2019-12-03 19:22:07 +0000

    added generic_search()

--- NAMS/database_layer/DBLayer.py
+++ NAMS/database_layer/DBLayer.py
@@ -9,3 +9,31 @@ class DBLayer:
 
 	def search():
 		pass
+
+
+        def generic_search(self, filename, filter_column, key_word, result_column="all"):
+            """
+            Function which searches a csv file for key_word in filter_column.
+            Returns value in specified result_column for each matched row.
+            Returns all values in row if result_column is "all" (default value).
+            """
+            filename = f'csv_files/{filename}'
+
+            csv_file = open(filename, "r")
+            csv_dict = DictReader(csv_file)  # Loads rows into dictionary with column names as keys.
+
+            results = [ ]
+
+            for row in csv_dict:
+
+                # Finds row with value in filter_column matching key_word.
+                if row[filter_column] == key_word:
+
+                    if result_column == "all":
+                        results.append([value for key, value in row.items()])
+
+                    else:
+                        results.append(row[result_column])
+
+            csv_file.close()
+            return results
