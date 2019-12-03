2566582a60cf2421167f5738df910d0985a96ecc
peturdanielthordarson <petur19@ru.is>
2019-12-03 11:18:49 +0000

    syntax fixing

--- NAMS/model_classes.py
+++ NAMS/model_classes.py
@@ -2,82 +2,82 @@
 from random import randint
 
 class Rtrip:
-	"""
-	Model class for round trips (flight from Iceland and flight back to Iceland).
-	"""
-	def __init__(self):
-		self.__valid_bool = False
-		self.__dest_str = ""
-		self.__rtime_int = 0
-		self.__passenger_count_int = ""
-		self.__airplane_type = ""		# Instance of class Airplane
-		self.__passenger_count_int = 0
-		self.__start_time_str = ""
-		self.__return_time_str = ""
+    """
+    Model class for round trips (flight from Iceland and flight back to Iceland).
+    """
+    def __init__(self):
+        self.__valid_bool = False
+        self.__dest_str = ""
+        self.__rtime_int = 0
+        self.__passenger_count_int = ""
+        self.__airplane_type_str = ""		# Instance of class Airplane
+        self.__passenger_count_int = 0
+        self.__start_time_str = ""
+        self.__return_time_str = ""
 
-	def set_valid(self):
-		if self.__dest_str != "" and self.__rtime_int != 0 and self.__start_time_str != "":
-			self.__valid_bool = True
-			return True
-		else:
-			return False
+    def set_valid(self):
+        if self.__dest_str != "" and self.__rtime_int != 0 and self.__start_time_str != "":
+            self.__valid_bool = True
+            return True
+        else:
+            return False
 
-	def set_dest_str(self, dest_str):
-		if dest_str.replace(" ", "").isalpha():
-			self.__dest_str = dest_str
-			return True
-		else:
-			return False
+    def set_dest_str(self, dest_str):
+        if dest_str.replace(" ", "").isalpha():
+            self.__dest_str = dest_str
+            return True
+        else:
+            return False
 
-	def get_dest_str(self):
-		return self.__dest_str
+    def get_dest_str(self):
+        return self.__dest_str
 
-	def set_rtime_int(self, rtime_int):  # Viljum við ekki reikna Rtrip time utfra destination og biðtíma?
-		if rtime_int.isdecimal:
-			self.__rtime_int = rtime_int
-			return True
-		else:
-			return False
+    def set_rtime_int(self, rtime_int):  # Viljum við ekki reikna Rtrip time utfra destination og biðtíma?
+        if rtime_int.isdecimal:
+            self.__rtime_int = rtime_int
+            return True
+        else:
+            return False
 
-	def get_rtime_int(self):
-		return self.__rtime_int
+    def get_rtime_int(self):
+        return self.__rtime_int
 
-	def set_airplane_type(self, airplane_type_str):
-		self.__airplane_type_str = airplane_type_str
-		return True
+    def set_airplane_type(self, airplane_type_str):
+        self.__airplane_type_str = airplane_type_str
+        return True
 
-	def get_airplane_type(self):
-		return self.__airplane_type_str
+    def get_airplane_type(self):
+        return self.__airplane_type_str
 
-	def set_passenger_count_int(self, passenger_count_int):
-		if type(passenger_count_int) is int and passenger_count_int < self.__airplane_type.get_seat_count_int():
-			self.__passenger_count_int = passenger_count_int
-			return True
-		else:
-			return False
+    def set_passenger_count_int(self, passenger_count_int):
+        if type(passenger_count_int) is int and passenger_count_int < self.__airplane_type.get_seat_count_int():
+            self.__passenger_count_int = passenger_count_int
+            return True
+        else:
+            return False
 
-	def get_passenger_count_int(self):
-		return self.__passenger_count_int
+    def get_passenger_count_int(self):
+        return self.__passenger_count_int
 
-	def set_start_time_str(self, start_time_str):
-		if start_time_str.replace(" ", "").isalpha():	 # Checka lika hvort það sé í samræmi við ISO formattið
-			self.__start_time_str = start_time_str       #lika hægt að setja start time og date saman i einn datetime string, sama f. returndatetime
-			return True
-		else:
-			return False
+    def set_start_time_str(self, start_time_str):
+        if start_time_str.replace(" ", "").isalpha():	 # Checka lika hvort það sé í samræmi við ISO formattið
+            self.__start_time_str = start_time_str       #lika hægt að setja start time og date saman i einn datetime string, sama f. returndatetime
+            return True
+        else:
+            return False
 
-	def get_start_time_str(self):
-		return self.__start_time_str
+    def get_start_time_str(self):
+        return self.__start_time_str
 
-	def set_return_time_str(self, return_time_str):
-		if return_time_str.replace(" ", "").isalpha():	 # Checka lika hvort það sé í samræmi við ISO formattið
-			self.__return_time_str = return_time_str
-			return True
-		else:
-			return False
+    def set_return_time_str(self, return_time_str):
+        if return_time_str.replace(" ", "").isalpha():	 # Checka lika hvort það sé í samræmi við ISO formattið
+            self.__return_time_str = return_time_str
+            return True
+        else:
+            return False
 
-	def get_return_time_str(self):
-		return self.__return_time_str
+    def get_return_time_str(self):
+        return self.__return_time_str
 
 
 class Destination:
@@ -336,69 +336,71 @@ class Crew:
 
 
 class Airplane:
-	"""
-	Model class for airplanes.
-	"""
-	def __init__(self):
-		self.__seat_count_int = 0
-		self.__type_str = ""
-		self.__manuf_str = ""
+    """
+    Model class for airplanes.
+    """
+    def __init__(self):
+        self.__seat_count_int = 0
+        self.__type_str = ""
+        self.__manuf_str = ""
 
-	def set_seat_count_int(self, seat_count_int):
-		if seat_count_int.isdecimal:
-			self.__seat_count_int = seat_count_int
-			return True
-		else:
-			return False
+    def set_seat_count_int(self, seat_count_int):
+        if seat_count_int.isdecimal:
+            self.__seat_count_int = seat_count_int
+            return True
+        else:
+            return False
 
-	def get_seat_count_int(self):
-		return self.__seat_count_int
+    def get_seat_count_int(self):
+        return self.__seat_count_int
 
-	def set_type_str(self, type_str):
-		if type_str.replace(" ", "").isalpha():
-			self.__type_str = type_str
-			return True
-		else:
-			return False
+    def set_type_str(self, type_str):
+        if type_str.replace(" ", "").isalpha():
+            self.__type_str = type_str
+            return True
+        else:
+            return False
 
-	def get_type_str(self):
-		return self.__type_str
+    def get_type_str(self):
+        return self.__type_str
 
-	def set_manuf_str(self, manuf_str):
-		if manuf_str.replace(" ", "").isalpha():
-			self.__manuf_str = manuf_str
-			return True
-		else:
-			return False
+    def set_manuf_str(self, manuf_str):
+        if manuf_str.replace(" ", "").isalpha():
+            self.__manuf_str = manuf_str
+            return True
+        else:
+            return False
 
-	def get_manuf_str(self):
-		return self.__manuf_str
+    def get_manuf_str(self):
+        return self.__manuf_str
 
-class Departure():
-	def __init__(self):
-		self.start_time_int = 0
-		self.end_time_int = 0
-	
-	def set_start_time_int(self, start_time_int)
-		if start_time_int.isdecimal():
-			self.__start_time_int = start_time_int
-			return True
-		else:
-			return False
-	
-	def get_start_time_int(self):
-		return self.__start_time_int
-	
-	def set_end_time_int(self, end_time_int):
-		if end_time_int.isdecimal():
-			self.__end_time_int = end_time_int
-			return True
-		else:
-			return False
 
-	def get_end_time_int(self):
-		return self.__end_time_int
-		
+class Departure:
+    def __init__(self):
+        self.__start_time_int = 0
+        self.__end_time_int = 0
+
+    def set_start_time_int(self, start_time_int):
+        if start_time_int.isdecimal():
+            self.__start_time_int = start_time_int
+            return True
+        else:
+            return False
+
+    def get_start_time_int(self):
+        return self.__start_time_int
+
+    def set_end_time_int(self, end_time_int):
+        if end_time_int.isdecimal():
+            self.__end_time_int = end_time_int
+            return True
+        else:
+            return False
+
+    def get_end_time_int(self):
+        return self.__end_time_int
+
+
 class Staff:
     def __init__(self):
         self.__pilot_list = []
