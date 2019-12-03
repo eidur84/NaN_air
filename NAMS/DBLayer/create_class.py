cd23e4268f3da580d6c048f4b60f71dcd09a758d
Pecky95 <danielpall3@gmail.com>
2019-12-03 13:00:33 +0000

    ksdf

--- NAMS/DBLayer/create_class.py
+++ NAMS/DBLayer/create_class.py
@@ -6,3 +6,17 @@ class Create:
         self.__create_dest_str = ""
         self.__create_rtrip_str = ""
 
+def update_new_data_str(self):
+    pass
+
+def update_create_staff_str(self):
+    pass
+
+def update_create_airplane_str(self):
+    pass
+
+def update_create_dest_str(self):
+    pass
+
+def update_rtrip_str(self):
+    pass
--- NAMS/DBLayer/invalidate class.py
+++ NAMS/DBLayer/invalidate class.py
@@ -4,3 +4,15 @@ class Invalidate():
         self.__invalidate_airplane_str = ""
         self.__invalidate_dest_str = ""
         self.__invalidate_rtrip_str = ""
+
+def update_invalidate_staff_str(self):
+    pass
+
+def update_invalidate_airplane_str(self):
+    pass
+
+def update_invalidate_dest_str(self):
+    pass
+
+def update_invalidate_rtrip_str(self):
+    pass
