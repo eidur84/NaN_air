09e83d1de25f5ba0cefcea5ce7d3a3f4484d5727
peturdanielthordarson <petur19@ru.is>
2019-12-03 11:43:46 +0000

    DBLayer class added only with init function

---
+++ NAMS/DBLayer/DBLayer.py
@@ -0,0 +1,11 @@
+class DBLayer:
+	def __init__(self):
+		self.__input_data = ''
+		# self.__output_data =
+		self.__key_word = ''
+		self.__search_column = ''
+		self.__result_column = ''
+		self.__filenames = []
+
+	def search(self, user_input):
+		pass
