c4916d15b4ef4075d2f644611d0e3e42e57e03ff
peturdanielthordarson <petur19@ru.is>
2019-12-03 11:51:34 +0000

    syntax fixing

---
+++ NAMS/DBLayer/update_class.py
@@ -0,0 +1,15 @@
+class Update:
+	def __init__(self):
+		self.__new_data = {}
+
+	def update_staff(self):
+		pass
+
+	def update_airplane(self):
+		pass
+
+	def update_rtrip(self):
+		pass
+
+	def update_dest(self):
+		pass
