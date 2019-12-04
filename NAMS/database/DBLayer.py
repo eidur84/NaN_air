24ceef361d25d65963444d8980ed304d4ae378d4
Logi Eyjólfsson <logi19@ru.is>
2019-12-04 10:33:01 +0000

    fixed generic_search()

--- NAMS/database/DBLayer.py
+++ NAMS/database/DBLayer.py
@@ -1,3 +1,6 @@
+
+from csv import DictReader
+
 class DBLayer:
 
 	input_data = ''
@@ -11,29 +14,29 @@ class DBLayer:
 		pass
 
 
-        def generic_search(self, filename, filter_column, key_word, result_column="all"):
-            """
-            Function which searches a csv file for key_word in filter_column.
-            Returns value in specified result_column for each matched row.
-            Returns all values in row if result_column is "all" (default value).
-            """
-            filename = f'csv_files/{filename}'
+	def generic_search(filename, filter_column, key_word, result_column="all"):
+		"""
+		Function which searches a csv file for key_word in filter_column.
+		Returns value in specified result_column for each matched row.
+		Returns all values in row if result_column is "all" (default value).
+		"""
+		filename = f'csv_files/{filename}'
 
-            csv_file = open(filename, "r")
-            csv_dict = DictReader(csv_file)  # Loads rows into dictionary with column names as keys.
+		csv_file = open(filename, "r")
+		csv_dict = DictReader(csv_file)  # Loads rows into dictionary with column names as keys.
 
-            results = [ ]
+		results = [ ]
 
-            for row in csv_dict:
+		for row in csv_dict:
 
-                # Finds row with value in filter_column matching key_word.
-                if row[filter_column] == key_word:
+			# Finds row with value in filter_column matching key_word.
+			if row[filter_column] == key_word:
 
-                    if result_column == "all":
-                        results.append([value for key, value in row.items()])
+				if result_column == "all":
+					results.append([value for key, value in row.items()])
 
-                    else:
-                        results.append(row[result_column])
+				else:
+					results.append(row[result_column])
 
-            csv_file.close()
-            return results
+		csv_file.close()
+		return results
