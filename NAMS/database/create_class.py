4365577dcf6d5500722ca5ef155648b6696b1215
Pecky95 <danielpall3@gmail.com>
2019-12-04 11:15:11 +0000

    sdf

--- NAMS/database/create_class.py
+++ NAMS/database/create_class.py
@@ -1,3 +1,5 @@
+from csv import DictWriter
+
 class Create:
     new_data_str = ""
     create_staff_str = ""
@@ -10,14 +12,23 @@ class Create:
 	    pass
 
 	def update_create_staff_str():
-	    pass
+	    with open("Staff.csv", "a", newline='') as file:
+    		writer = csv.DictWriter(file)
+    		writer.writerow({"valdi" : ?, "ssn" : ? "staffID" : ?, "name" : ?, "job" : ?, "home" : ?, "landline" : ?, "gsm" : ?, "email" : ?, "license" : ?})
+
+
 
 	def update_create_airplane_str():
-	    path = "NaN_air\NAMS\csv_files\Airplanes.csv" + filename
+	  	with open("Airplanse.csv", "a", newline='') as file:
+    		writer = csv.DictWriter(file)
+    		writer.writerow({"airplaneID" : ?, "manufacturer" : ? "name" : ?, "type" : ?, "seat_count" : ?, "valid" : ?,
 
 	def update_create_dest_str():
-	    path = "NaN_air\NAMS\csv_files\Destinations.csv" + filename
+	  	with open("Destination.csv", "a", newline='') as file:
+    		writer = csv.DictWriter(file)
+    		writer.writerow({"valid" : ?, "ID" : ? "country" : ?, "city" : ?, "airport" : ?, "flight_time" : ?, "distance" : ?, "contact_name" : ? "contact_phone" 
 
 	def update_rtrip_str():
-	    path = "NaN_air\NAMS\csv_files\Departures.csv" + filename
-	    path = "NaN_air\NAMS\csv_files\Returnflights.csv" + filename
+	  	with open("RoundTrips.csv", "a", newline='') as file:
+    		writer = csv.DictWriter(file)
+    		writer.writerow({"valid" : ?, "past" : ? "direction" : ?, "flightID" : ?, "departingFrom" : ?, "arrivingAT" : ?, "departure" : ?, "arrival" : ? "aircraft_name"
