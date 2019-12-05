d07874440f88d003164a8ba7c6c93a9ee85c0f4c
Eiður Ágúst Egilsson <eidur14@ru.is>
2019-12-05 11:20:11 +0000

    eqrgsd

--- NAMS/model_classes/crew_class.py
+++ NAMS/model_classes/crew_class.py
@@ -1,72 +1,4 @@
 class Crew:
-<<<<<<< HEAD
-    """
-    Model class containing staff members for a particular flight
-    """
-    __valid_bool = False
-    __flightID_int = ""	
-    __headpilot_str = ''
-    __copilot_str = ''
-    __headsteward_str = ''
-    __otherstewards_strlst = []
-
-    def __init__(self, flightid, headpilot, copilot, headsteward, otherstewards):
-        self.__flightID_int = flightid		# vantar get/set methods
-        self.__headpilot_str = headpilot
-        self.__copilot_str = copilot
-        self.__headsteward_str = headsteward
-        self.__otherstewards_strlst = []
-
-    # !!Validation á klasabreytum fyrir database layerinn.. Vantar að klára!!
-
-    # def set_valid(self):
-    #    if self.__flightID_str.isalpha() == True and len(__flightID_str) == 0 and len(country_name_str) > 0 \
-	#	and self.__city_name_str.isalpha() == True and len(city_name_str) <= 60 and len(city_name_str) > 0 \
-	#	and self.__airport_name_str.isalpha() == True and len(airport_name_str) <= 60 and len(airport_name_str) > 0 \
-	#	and isinstance(__distance_int,int) and __distance_int > 0 \
-     #   and isinstance(__flight_time_int,int) and __flight_time_int > 0:	
-     #   	self.__valid_bool = True
-	#	else:
-	#		self.__valid_bool = False   	
-
-
-    def set_headpilot(self, name):
-        if name_str.replace(' ', '').isalpha() and len(name_str) <= 60:
-            self.__headpilot_str = name
-            return True
-        else:
-            return False
-
-    def get_headpilot(self):
-        return self.__headpilot_str
-
-    def set_copilot(self, name):
-        if name_str.replace(' ', '').isalpha() and len(name_str) <= 60:
-            self.__copilot_str = name
-            return True
-        else:
-            return False
-
-    def get_copilot(self):
-        return self.__copilot_str
-
-    def set_headsteward(self, name):
-        if name.replace(' ', '').isalpha() and len(name) <= 60:
-            self.__headsteward_str = name
-            return True
-        else:
-            return False
-
-    def get_headsteward(self):
-        return self.__headsteward_str
-
-    def set_stewards(self, name):
-        if name.replace(' ', '').isalpha() and len(name) <= 60:
-            self.__otherstewards.append(name)
-            return True
-        else:
-            return False
-=======
 	"""
 	Model class containing staff members for a particular flight.
 	"""
@@ -114,4 +46,3 @@ class Crew:
 			return True
 		else:
 			return False
->>>>>>> 6a82a579c2d1da4198142fb7d453c8a7ce7fadc8
--- NAMS/model_classes/employee_class.py
+++ NAMS/model_classes/employee_class.py
@@ -1,157 +1,25 @@
 
 class Employee:
-<<<<<<< HEAD
-    """
-    Model class for employees.
-    """
-
-    # Klasabreytur
-
-    __valid_bool = False
-    __name_str = ""
-    __ssn_str = ""
-    __address_str = ""
-    __landline_str = ""
-    __gsm_str = ""
-    __email_str = ""
-    __job_str = ""
-    __allowed_planes_list = []
-
-
-    #Constructor
-    #Erum við ekki að vinna með eina leyfilega flugvél??
-
-    def __init__(self):
-        self.__valid_bool = False
-        self.__name_str = ""
-        self.__ssn_str = ""
-        self.__address_str = ""
-        self.__landline_str = ""
-        self.__gsm_str = ""
-        self.__email_str = ""
-        self.__job_str = ""
-        self.__allowed_planes_list = []
-
-    def set_valid(self):
-        pass
-
-    def set_name(self, name):
-        if name.replace(" ", "").isalpha() and len(name) < 60:
-            self.__name_str = name
-            return True
-        else:
-            return False
-
-    def get_name(self):
-        return self.__name_str
-
-    # Þarf að setja inn löglega kennitölu
-
-    def set_ssn(self, ssn_str):  # INCOMPLETE CHECKS~~~~~~~~~~~~~~
-        if ssn_str.isdecimal():
-            if int(ssn_str[2:4]) <= 12:
-                if ssn_str[2:4] == "02":  # February
-                    if int(ssn_str[:2]) <= 29:
-                        self.__ssn_str = ssn_str
-                        return True
-
-                elif ssn_str[2:4] in ["04", "06", "09", "11"]:
-
-                    if int(ssn_str[:2]) <= 30:
-                        self.__ssn_str = ssn_str
-                        return True
-
-                else:
-                    if int(ssn_str[:2]) <= 31:
-                        self.__ssn_str = ssn_str
-                        return True
-        return False
-
-    def get_ssn(self):
-        return self.__ssn_str
-
-    def set_address(self, address):
-        if len(address) < 60:
-            self.__address_str = address
-            return True
-        else:
-            return False
-
-    def get_address(self):
-        return self.__address_str
-
-    def set_landline(self, landline:
-        if landline.isdecimal():
-            self.__landline_str = landline
-            return True
-        else:
-            return False
-
-    def get_landline(self):
-        return self.__landline_str
-
-    def set_gsm(self, gsm):
-        if gsm.isdecimal():
-            self.__gsm_str = gsm
-            return True
-        else:
-            return False
-
-    def get_gsm(self):
-        return self.__gsm_str
-
-    # Þarf mögulega að útfæra nákvæmari test á emailið
-
-    def set_email(self, email_str):
-        if "@" in email_str and "." in email_str:
-            if email_str.replace("@", "").replace(".", "").isalpha():
-                self.__email_str = email_str
-                return True
-
-        return False
-
-    def get_email(self):
-        return self.__email_str
-
-    def set_job(self, job_str):
-        if job_str.isalpha():
-            self.__job_str = job_str
-            return True
-        else:
-            return False
-
-    def get_job(self):
-        return self.__job_str
-
-    def set_allowed_planes(self, airplane_type):
-        # IMPLEMENT CHECK
-        self.__allowed_planes_list.append(airplane_type)
-        return True
-
-    def get_allowed_planes(self):
-        return self.__allowed_planes_list
-
-=======
 	"""
 	Model class for employees.
 	"""
-	def __init__(self):
+	def __init__(self, name = "", ssn = "", address = "", landline = "", gsm = "", email = "", job = "", allowed = ""):
 		self.__valid_bool = False
-		self.__name_str = ""
-		self.__ssn_str = ""
-		self.__address_str = ""
-		self.__landline_str = ""
-		self.__gsm_str = ""
-		self.__email_str = ""
-		self.__job_str = ""
-		self.__allowed_plane_str = ""
+		self.__name_str = self.set_name(name)
+		self.__ssn_str = self.set_ssn(ssn)
+		self.__address_str = self.set_address(address)
+		self.__landline_str = self.set_landline(landline)
+		self.__gsm_str = self.set_gsm(gsm)
+		self.__email_str = self.set_email(email)
+		self.__job_str = self.set_job(job)
+		self.__allowed_plane_str = self.set_allowed_plane
 
 	def set_valid(self):
 		self.__valid_bool = True
 		return True
 
 	def set_name(self, name_str):
-		if name_str.replace(" ", "").isalpha() and len(name_str) < 60:
+		if name_str.replace(" ", "").isalpha() and len(name_str) < 60 and len(name_str) > 0:
 			self.__name_str = name_str
 			return True
 		else:
@@ -161,7 +29,7 @@ class Employee:
 		return self.__name_str
 
 	def set_ssn(self, ssn_str):  # INCOMPLETE CHECKS~~~~~~~~~~~~~~
-		if ssn_str.isdecimal():
+		if ssn_str.isdecimal(): 
 			if int(ssn_str[2:4]) <= 12:
 				if ssn_str[2:4] == "02":  # February
 					if int(ssn_str[:2]) <= 29:
@@ -234,7 +102,7 @@ class Employee:
 	def get_job(self):
 		return self.__job_str
 
-	def set_allowed_planes(self, airplane_type):
+	def set_allowed_plane(self, airplane_type):
 		# IMPLEMENT CHECK
 		self.__allowed_plane_str = airplane_type
 		return True
@@ -259,4 +127,3 @@ class Employee:
 
 		attribute_dict = dict(zip(column_names, attributes))
 		return attribute_dict
->>>>>>> 6a82a579c2d1da4198142fb7d453c8a7ce7fadc8
