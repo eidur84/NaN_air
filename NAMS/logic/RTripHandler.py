49bac05beeb4953617ab213fca55d8cb2d1a72a2
peturdanielthordarson <petur19@ru.is>
2019-12-05 11:25:32 +0000

    Skeleton for RTripHandler added

---
+++ NAMS/logic/RTripHandler.py
@@ -0,0 +1,19 @@
+from csv import DictWriter
+from logic.BLLayer import BLLayer
+
+class RTripHandler():
+
+	def create_flight(self):
+		pass
+
+	def calculate_flight_time(self):
+		pass
+
+	def update_flight(self):
+		pass
+
+	def read_flight(self):
+		pass
+
+	def invalidate_flight(self):
+		pass
