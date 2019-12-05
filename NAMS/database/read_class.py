2f46333e9c67132d61194aa728f6802f907d4d55
peturdanielthordarson <petur19@ru.is>
2019-12-05 10:48:39 +0000

    Invalidate_class pathing added

--- NAMS/database/read_class.py
+++ NAMS/database/read_class.py
@@ -1,5 +1,5 @@
 from csv import DictReader
-from database.DBLayer_class import DBLayer
+from database.database_layer import DBLayer
 
 class Read(DBLayer):
 	read_staff_str = ""
@@ -7,18 +7,18 @@ class Read(DBLayer):
 	read_dest_str = ""
 	read_rtrip_str = ""
 
-	def update_read_staff_str():
-		staff_read = DBLayer.generic_search('staff.csv', filter_column, read_staff_str)
+	def update_read_staff_str(employee, filter_column, read_name_str):
+		staff_read = DBLayer.generic_search('Staff.csv', filter_column, read_name_str)
 		return staff_read
 
-	def update_airplane_str():
+	def update_airplane_str(airplane, filter_column):
 		airplane_read = DBLayer.generic_search('Airplanes.csv', filter_column, read_airplane_str)
 		return airplane_read
 
-	def update_read_dest_str():
+	def update_read_dest_str(destination, filter_column):
 		destination_read = DBLayer.generic_search('Destinations.csv', filter_column, read_dest_str)
 		return destination_read
 
-	def update_read_rtrip_str():
+	def update_read_rtrip_str(rtrip, filter_column):
 		rtrip_read = DBLayer.generic_search('RoundTrips.csv', filter_column, read_rtrip_str)
 		return rtrip_read
--- NAMS/run.py
+++ NAMS/run.py
@@ -1,3 +1,4 @@
 from database.create_class import Create
 from database.database_layer import DBLayer
+from database.read_class import Read
 from model_classes.employee_class import Employee
