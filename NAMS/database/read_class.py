72ddcbf3d83a7d3022f0efd1f2a68223614eef12
Logi Eyjólfsson <logi19@ru.is>
2019-12-05 10:58:12 +0000

    fixed Read()

--- NAMS/database/read_class.py
+++ NAMS/database/read_class.py
@@ -1,24 +1,23 @@
 from csv import DictReader
 from database.database_layer import DBLayer
 
-class Read(DBLayer):
-	read_staff_str = ""
-	read_airplane_str = ""
-	read_dest_str = ""
-	read_rtrip_str = ""
 
-	def update_read_staff_str(employee, filter_column, read_name_str):
-		staff_read = DBLayer.generic_search('Staff.csv', filter_column, read_name_str)
+class Read(DBLayer):
+
+	def read_staff(filter_column, key_word):
+		staff_read = DBLayer.generic_search('Staff.csv', filter_column, key_word)
 		return staff_read
 
-	def update_airplane_str(airplane, filter_column):
-		airplane_read = DBLayer.generic_search('Airplanes.csv', filter_column, read_airplane_str)
+	def read_airplane(filter_column, key_word):
+		airplane_read = DBLayer.generic_search('Airplanes.csv', filter_column, key_word)
 		return airplane_read
 
-	def update_read_dest_str(destination, filter_column):
-		destination_read = DBLayer.generic_search('Destinations.csv', filter_column, read_dest_str)
+	def read_destination(filter_column, key_word):
+		destination_read = DBLayer.generic_search('Destinations.csv', filter_column, key_word)
 		return destination_read
 
-	def update_read_rtrip_str(rtrip, filter_column):
+'''
+	def read_rtrip(rtrip, filter_column):
 		rtrip_read = DBLayer.generic_search('RoundTrips.csv', filter_column, read_rtrip_str)
 		return rtrip_read
+'''
