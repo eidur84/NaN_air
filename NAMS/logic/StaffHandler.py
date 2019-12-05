721addd695d940ca7ef2cd419a07770e1257dd85
peturdanielthordarson <petur19@ru.is>
2019-12-05 13:33:05 +0000

    StaffHandler create_staff skeleton added

--- NAMS/logic/StaffHandler.py
+++ NAMS/logic/StaffHandler.py
@@ -6,34 +6,29 @@ class StaffHandler():
 	def create_staff(DATA):
 		if len(DATA) > 0:
 			bool = Employee.set_name(DATA)
-			return bool
 
 		if len(DATA) > 0:
 			bool = Employee.set_ssn(DATA)
-			return bool
+
 		if len(DATA) > 0:
 			bool = Employee.set_address(DATA)
-			return bool
 
 		if len(DATA) > 0:
 			bool = Employee.set_landline(DATA)
-			return bool
 
 		if len(DATA) > 0:
 			bool = Employee.set_gsm(DATA)
-			return bool
 
 		if len(DATA) > 0:
 			bool = Employee.set_email(DATA)
-			return bool
 
 		if len(DATA) > 0:
 			bool = Employee.set_job(DATA)
-			return bool
 
-		if len(DATA) > 0
+		if len(DATA) > 0:
 			bool = Employee.set_allowed_planes(DATA)
-			return bool
+
+		return bool
 
 	def license_check():
 		pass
