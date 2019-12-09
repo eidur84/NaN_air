060f60f4704c6eb1d0ad9a0ddc2a30d5706eca77
peturdanielthordarson <petur19@ru.is>
2019-12-09 20:43:45 +0000

    Useless code in FlightDate removed. Functions now 3 instead of 4

--- secondNAMS/model_classes/flightdate_class.py
+++ secondNAMS/model_classes/flightdate_class.py
@@ -4,10 +4,7 @@ import datetime as dt
 class FlightDate:
 
 	def __init__(self, datetime):
-		self.datetime = FlightDate.make_isoformat(datetime)
-
-	def make_isoformat(datetime):
-		return datetime.isoformat()
+		self.datetime = datetime.isoformat()
 
 	def short_display(self):
 		printobject = f"{self.datetime}"
