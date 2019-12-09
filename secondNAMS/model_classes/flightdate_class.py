eda3778a9ab344135e95e61906aedb9a6044f19a
peturdanielthordarson <petur19@ru.is>
2019-12-09 15:06:37 +0000

    FlightDate model class added

---
+++ secondNAMS/model_classes/flightdate_class.py
@@ -0,0 +1,13 @@
+import datetime as dt
+
+class FlightDate:
+
+	def __init__(self, datetime):
+		self.datetime = FlightDate.make_isoformat(datetime)
+
+	def make_isoformat(datetime):
+		return datetime.isoformat()
+
+	def short_display(self):
+		printobject = f"{self.datetime}"
+		return printobject
